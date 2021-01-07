a4c4ae9f8c75b55cda659b302371ab7cd002e389
Abdurrahman <a.eshibiny@gmail.com>
2021-01-07 23:17:21 +0200

    Separated the doctor schedule in a file other than the main for modularity

--- HMS/lib/main.dart
+++ HMS/lib/main.dart
@@ -1,12 +1,6 @@
-import 'widgets/DoctorAppointment.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import './widgets/CustomDrawer.dart';
+import 'package:HMS/pages/DoctorSchedule.dart';
 import 'package:flutter/material.dart';
-import './widgets/CustomFloatingActionButton.dart';
-import './widgets/CustomAppBar.dart';
 import 'package:firebase_core/firebase_core.dart';
-import './services/DbService.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,41 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HMS',
-      home: HomePage(),
+      home: DoctorSchedule(),
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: DbService().getPatients,
-        builder: (context, snapshot) {
-          return Scaffold(
-            appBar: constructCustomAppBar(() {}, 'Suxess', '0123456789'),
-            drawer: CustomDrawer(),
-            floatingActionButton: CustomFloatingActionButton(() {}),
-            backgroundColor: Color(0xFF035F6D),
-            body: ListView.builder(
-              itemCount: snapshot.data.docs.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: DoctorAppointment(
-                    cardId: snapshot.data.docs[index].data()['cardId'],
-                    isAppointment: true,
-                    titleOfCardApp: snapshot.data.docs[index].data()['name'],
-                    date: snapshot.data.docs[index].data()['date'],
-                    doctor: 'Dr. Fancy Coats',
-                    department: snapshot.data.docs[index].data()['department'],
-                    treatment: snapshot.data.docs[index].data()['treatment'],
-                    time: snapshot.data.docs[index].data()['time'],
-                    diagnose: snapshot.data.docs[index].data()['diagnosis'],
-                  ),
-                );
-              },
-            ),
-          );
-        });
   }
 }
