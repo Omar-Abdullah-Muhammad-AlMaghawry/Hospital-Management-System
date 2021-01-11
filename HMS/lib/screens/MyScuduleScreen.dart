cee6185610c7b4e5fbcbe5428853cd460d15fad9
Abdurrahman <a.eshibiny@gmail.com>
2021-01-11 04:36:02 +0200

    Added and fixed the schedule page of the doctor

---
+++ HMS/lib/screens/MyScuduleScreen.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../widget/scudule.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyScuduleScreen extends StatefulWidget {
+  static const nameRoute = "/my-scudule";
+  @override
+  _MyScuduleScreenState createState() => _MyScuduleScreenState();
+}
+
+class _MyScuduleScreenState extends State<MyScuduleScreen> {
+  String _imageScUrl;
+  final _user = FirebaseAuth.instance.currentUser;
+  void _pickImage() async {
+    final pickedImageFile = await ImagePicker.pickImage(
+        source: ImageSource.camera, imageQuality: 50, maxWidth: 300);
+    final _ref =
+        FirebaseStorage.instance.ref().child("Scudule").child("Scudule.jpg");
+    await _ref.putFile(pickedImageFile);
+    final _imageUrl = await _ref.getDownloadURL();
+    await FirebaseFirestore.instance
+        .collection("doctors")
+        .document(_user.uid)
+        .update({
+      "sceduleImageUrl": _imageUrl,
+    });
+    setState(() {
+      _imageScUrl = _imageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyScudule"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.of(context).pop();
+              FirebaseAuth.instance.signOut();
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+          future: FirebaseFirestore.instance
+              .collection("doctors")
+              .document(_user.uid)
+              .get(),
+          builder: (context, snapshot) {
+            final doctorDocs = snapshot.data;
+            if (doctorDocs["sceduleImageUrl"] != null)
+              return Scudule(doctorDocs["sceduleImageUrl"]);
+            else
+              Center();
+          }),
+
+      /// drawer: ,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pickImage,
+        child: Icon(
+          Icons.camera_alt,
+          color: Theme.of(context).bottomAppBarColor,
+        ),
+      ),
+    );
+  }
+}
